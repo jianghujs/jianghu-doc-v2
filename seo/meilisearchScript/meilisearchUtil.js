fea1d09ab742c8aa17528f8340d73320a39b9267
colin <colin>
2024-03-30 13:44:29 +0800

    meilisearch

---
+++ seo/meilisearchScript/meilisearchUtil.js
@@ -0,0 +1,54 @@
+'use strict';
+
+require('dotenv').config({ path: './meilisearchScript/.env' });
+const { MeiliSearch } = require('meilisearch');
+
+const client = new MeiliSearch({
+  host: process.env.MEILISEARCH_HOST_URL,
+  apiKey: process.env.MEILISEARCH_API_KEY,
+});
+
+
+async function getKeys() {
+  const keys = await client.getKeys();
+  console.log("meilisearchScript:getKeys success", keys);
+}
+
+async function createKey() {
+  const createKeyRes = await client.createKey({
+    description: '只读的api key[cn_openjianghu_org_public]',
+    actions: ['search'],
+    indexes: ['cn_openjianghu_org_public', 'cn_openjianghu_org_all'],
+    expiresAt: '2030-01-01T00:00:00Z'
+  })
+  console.log("meilisearchScript:createKey success", createKeyRes);
+}
+
+async function deleteKey() {
+  const apiKey = 'xxxxx';
+  await client.deleteKey(apiKey)
+  console.log("meilisearchScript:deleteKey success");
+}
+
+
+async function deleteIndex() {
+  const index_uid = 'test_uid';
+  await client.deleteIndex(index_uid);
+  console.log("meilisearchScript:deleteIndex success");
+}
+
+async function deleteAllDocuments() {
+  const index_uid = 'test_uid';
+  await client.index(index_uid).deleteAllDocuments();
+  console.log("meilisearchScript:deleteAllDocuments success");
+}
+
+const method = process.argv[2];
+switch (method) {
+  case 'getKeys': getKeys(); break;
+  case 'createKey': createKey(); break;
+  case 'deleteKey': deleteKey(); break;
+  case 'deleteIndex': deleteIndex(); break;
+  case 'deleteAllDocuments': deleteAllDocuments(); break;
+  default: console.error('method不支持', { method }); break;
+}
