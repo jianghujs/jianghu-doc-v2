324e78570c271a2b3213ec94d09a3684c4702750
geekape <861013016@qq.com>
2024-05-15 22:26:00 +0800

    tailwindcss config

---
+++ seo/app/public/tailwindcss/tailwindcss.config.js
@@ -0,0 +1,10 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'green-500': '#42b883',
+                'green-100': '#F8FCFB',
+            }
+        }
+    }
+}
